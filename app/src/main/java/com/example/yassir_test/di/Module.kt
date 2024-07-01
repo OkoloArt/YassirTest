0923799ee4c2ae8d8fd23d0e0659490e17801e75
Arthur <okoloarthur4@gmail.com>
2024-07-01 16:14:07 +0100

    refactor module

--- app/src/main/java/com/example/yassir_test/di/Module.kt
+++ app/src/main/java/com/example/yassir_test/di/Module.kt
@@ -1,6 +1,5 @@
 package com.example.yassir_test.di
 
-import android.content.Context
 import com.example.yassir_test.data.repository.MovieRepo
 import com.example.yassir_test.data.repository.MovieRepoImpl
 import com.example.yassir_test.network.ApiService
@@ -8,23 +7,10 @@ import com.example.yassir_test.network.authHttpClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
 import javax.inject.Named
 import javax.inject.Singleton
-
-//@Module
-//@InstallIn(SingletonComponent::class)
-//object DataStoreModule {
-//
-//    @Provides
-//    @Singleton
-//    fun provideDataStoreRepository(
-//        @ApplicationContext context: Context
-//    ) = DataStoreRepo(context = context)
-//
-//}
 
 @Module
 @InstallIn(SingletonComponent::class)
